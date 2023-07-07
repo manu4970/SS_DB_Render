d45c43f077a803e712ce607d7a7fe3c55e97191a
manu4970 <manuelperez497078@gmail.com>
2023-07-07 19:29:56 -0400

    update

--- app.py
+++ app.py
@@ -51,9 +51,13 @@ def signUp():
     data = request.get_json()
 
     user = User.query.filter_by(email=data["email"]).first()
+    print(user,data)
 
     if user:
         return jsonify({"msg": "User already exists"}), 401
+    if user.password == "":
+        return jsonify({"msg": "Password is empty"}), 401
+
 
     hashed_password = generate_password_hash(data["password"], method='sha256')
 
