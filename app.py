8af69ee6ae4e9d81ae7f43c105a3d9b4ad88697e
manu4970 <manuelperez497078@gmail.com>
2023-07-10 18:56:46 -0400

    se actulaiza model canchas

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ss_db_render_yjbh_user:QOcQgtp8HkTjPGhDC3E0XXAvfEVKxpAs@dpg-cim6lg5ph6eg6kd230dg-a.oregon-postgres.render.com/ss_db_render_yjbh"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ss_db_render_ggq2_user:GCwBzPUDd0StxHcQRLjzGKLV2Xu96wRc@dpg-cim8p5dgkuvgvhe8fas0-a.oregon-postgres.render.com/ss_db_render_ggq2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'secret'
 CORS(app)
